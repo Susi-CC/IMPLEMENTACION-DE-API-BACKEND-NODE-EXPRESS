3b29f979182149742d7b3d68f0d6d494e40839b8
Susana Contreras <176187436+Susi-CC@users.noreply.github.com~>
2025-01-14 18:07:46 -0300

    estructura lista

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,12 +4,12 @@ module.exports = {
   HOST: process.env.HOST,
   USER: process.env.USER,
   PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
+  DB: 'db_jwtbootcamp',
   dialect: process.env.DIALECT,
   pool: {
-    max: parseInt(process.env.POOL_MAX, 10),
-    min: parseInt(process.env.POOL_MIN, 10),
-    acquire: parseInt(process.env.POOL_ACQUIRE, 10),
-    idle: parseInt(process.env.POOL_IDLE, 10),
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
   }
 };
